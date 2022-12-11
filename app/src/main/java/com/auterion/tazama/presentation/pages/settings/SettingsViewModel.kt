e25a9d5c8f5c30cd4be24242122eb6bfc9655963
Roman Bapst <bapstroman@gmail.com>
2022-12-11 12:05:23 +0300

    added option to select map type
    Signed-off-by: Roman Bapst <bapstroman@gmail.com>

--- app/src/main/java/com/auterion/tazama/presentation/pages/settings/SettingsViewModel.kt
+++ app/src/main/java/com/auterion/tazama/presentation/pages/settings/SettingsViewModel.kt
@@ -8,10 +8,22 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor() : ViewModel() {
-    val _fakeVehiclePosition = MutableStateFlow(false)
+    private val _fakeVehiclePosition = MutableStateFlow(false)
     val fakeVehiclePosition = _fakeVehiclePosition.asStateFlow()
+
+    private val _mapTypes = listOf("Satellite", "Normal", "Hybrid")
+    val mapTypes = _mapTypes
+
+    private var _currentMapType = MutableStateFlow("Satellite")
+    val currentMapType = _currentMapType.asStateFlow()
 
     fun setFakeVehiclePosition(fake: Boolean) {
         _fakeVehiclePosition.value = fake
+    }
+
+    fun setSatelliteMap(mapType: String) {
+        if (_mapTypes.contains(mapType)) {
+            _currentMapType.value = mapType
+        }
     }
 }
