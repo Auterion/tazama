793fedc93471d47f63ecc4d4c3c634f7e637a911
Jonas Vautherin <jonas.vautherin@gmail.com>
2023-04-30 00:23:20 +0200

    Add Maplibre to Survey

--- survey/app/src/main/java/com/auterion/tazama/survey/MainActivity.kt
+++ survey/app/src/main/java/com/auterion/tazama/survey/MainActivity.kt
@@ -11,33 +11,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.auterion.tazama.survey.ui.theme.TazamasurveyTheme
+import com.mapbox.mapboxsdk.geometry.LatLng
+import org.maplibre.compose.Circle
+import org.maplibre.compose.MapLibre
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TazamasurveyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                MapLibre(modifier = Modifier.fillMaxSize()) {
+                    Circle(
+                        center = LatLng(0.0, 0.0),
+                        draggable = true,
+                        color = "Red",
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TazamasurveyTheme {
-        Greeting("Android")
     }
 }
