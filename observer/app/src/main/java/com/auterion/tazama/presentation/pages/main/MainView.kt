e029bbab34b36ddcc043f239d357e4524e1c1a63
Jonas Vautherin <jonas.vautherin@gmail.com>
2023-05-11 01:05:07 +0200

    observer: improve orientation handling

--- observer/app/src/main/java/com/auterion/tazama/presentation/pages/main/MainView.kt
+++ observer/app/src/main/java/com/auterion/tazama/presentation/pages/main/MainView.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -28,7 +29,6 @@ import com.auterion.tazama.libui.presentation.components.VehicleMapMarker
 import com.auterion.tazama.libui.presentation.pages.main.SwappableView
 import com.auterion.tazama.libui.presentation.pages.main.TelemetryDisplayNumber
 import com.auterion.tazama.libui.presentation.pages.main.TelemetryInfo
-import com.auterion.tazama.libui.presentation.util.Orientation
 import com.auterion.tazama.libvehicle.Degrees
 import com.auterion.tazama.libvehicle.PositionAbsolute
 import com.auterion.tazama.observer.R
@@ -46,9 +46,7 @@ fun MainView(
     vehicleViewModel: VehicleViewModel,
     player: ExoPlayer
 ) {
-    val orientation = Orientation.observeOrientation()
-
-    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) {
         Box(modifier = Modifier.fillMaxSize()) {
             TelemetryComposable(
                 modifier = Modifier
