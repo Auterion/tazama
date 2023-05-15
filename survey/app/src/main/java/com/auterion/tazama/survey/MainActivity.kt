d3b6ebbed4eca123d7b092c69a49d770a096f52c
Roman Bapst <bapstroman@gmail.com>
2023-05-15 13:49:15 +0300

    progress on polygon, now can move marker and it will be continuous
    Signed-off-by: Roman Bapst <bapstroman@gmail.com>

--- survey/app/src/main/java/com/auterion/tazama/survey/MainActivity.kt
+++ survey/app/src/main/java/com/auterion/tazama/survey/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
 import com.auterion.tazama.survey.ui.theme.TazamasurveyTheme
 import com.mapbox.mapboxsdk.geometry.LatLng
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import org.maplibre.compose.MapLibre
 
 class MainActivity : ComponentActivity() {
@@ -18,20 +20,19 @@ class MainActivity : ComponentActivity() {
             TazamasurveyTheme {
                 val survey = Survey()
 
-                val vertices = survey.vertices.collectAsState()
+                val vertices = survey.verticeFlow.collectAsState()
 
                 MapLibre(modifier = Modifier.fillMaxSize()) {
                     SurveyPolygon(
                         vertices.value.toMutableList(),
-                        onVerticesChanged = { survey.handleVerticesChanged(it) },
+                        onVerticesTranslated = { survey.handleVerticesTranslated(it) },
                         onVerticeAtIndexChanged = { index, vertice ->
                             survey.handleVerticeChanged(
                                 index,
                                 vertice
                             )
-                        }, onVerticeInsert = { index, location ->
-                            survey.insertVertice(index, location)
-                        })
+                        }
+                    )
                 }
             }
         }
@@ -39,35 +40,76 @@ class MainActivity : ComponentActivity() {
 }
 
 class Survey() {
-    val vertices: MutableStateFlow<List<Vertice>> = MutableStateFlow(listOf())
+    private var vertices = mutableStateListOf<Vertice>()
+    private var _verticeFlow = MutableStateFlow(vertices)
+    val verticeFlow = _verticeFlow.asStateFlow()
 
     init {
-        val newList = mutableListOf<Vertice>()
-        newList.add(Vertice(LatLng(4.8, 46.0), 8.0f, true, "Gray"))
-        newList.add(Vertice(LatLng(4.8, 46.2), 8.0f, true, "Gray"))
-        newList.add(Vertice(LatLng(4.6, 46.2), 8.0f, true, "Gray"))
-        newList.add(Vertice(LatLng(4.6, 46.0), 8.0f, true, "Gray"))
+        vertices = mutableStateListOf<Vertice>()
+        vertices.add(Vertice(LatLng(4.8, 46.0), 8.0f, true, "Gray", sequence = 0))
+        vertices.add(Vertice(LatLng(4.8, 46.0), 8.0f, true, "Gray", VerticeRole.INSERTER, 1))
+        vertices.add(Vertice(LatLng(4.8, 46.2), 8.0f, true, "Gray", sequence = 2))
+        vertices.add(Vertice(LatLng(4.8, 46.0), 8.0f, true, "Gray", VerticeRole.INSERTER, 3))
+        vertices.add(Vertice(LatLng(4.6, 46.2), 8.0f, true, "Gray", sequence = 4))
+        vertices.add(Vertice(LatLng(4.8, 46.0), 8.0f, true, "Gray", VerticeRole.INSERTER, 5))
+        vertices.add(Vertice(LatLng(4.6, 46.0), 8.0f, true, "Gray", sequence = 6))
+        vertices.add(Vertice(LatLng(4.8, 46.0), 8.0f, true, "Gray", VerticeRole.INSERTER, 7))
 
-        vertices.value = newList
+        _verticeFlow.value = vertices
     }
 
-    fun handleVerticesChanged(vertices: MutableList<Vertice>) {
-        val newList: MutableList<Vertice> = vertices
-        this.vertices.value = newList
+    fun handleVerticesTranslated(coords: MutableList<LatLng>) {
+        for (i in 0..vertices.size - 1) {
+            if (vertices[i].role == VerticeRole.DRAGGER) {
+                vertices[i] = vertices[i].copy(location = coords[vertices[i].sequence / 2])
+            }
+        }
+
+        _verticeFlow.value = vertices
     }
 
-    fun handleVerticeChanged(index: Int, vertice: Vertice) {
+    fun handleVerticeChanged(index: Int, latLng: LatLng) {
+        val changedVertice = vertices[index]
 
-        val newList: MutableList<Vertice> = vertices.value.toMutableList()
-        newList[index] = vertice
-        this.vertices.value = newList
+        if (changedVertice.role == VerticeRole.DRAGGER) {
+            vertices[index] = changedVertice.copy(location = latLng)
+        } else {
+            val sequence = changedVertice.sequence
+            vertices[index] = changedVertice.copy(
+                location = latLng,
+                role = VerticeRole.DRAGGER,
+                sequence = sequence + 1
+            )
+
+            vertices.add(
+                Vertice(
+                    location = LatLng(),
+                    radius = 8.0f,
+                    draggable = true,
+                    color = "Red",
+                    VerticeRole.INSERTER,
+                    sequence
+                )
+            )
+            vertices.add(
+                Vertice(
+                    location = LatLng(),
+                    radius = 8.0f,
+                    draggable = true,
+                    color = "Red",
+                    VerticeRole.INSERTER,
+                    sequence + 2
+                )
+            )
+
+            for (i in 0..vertices.size - 3) {
+                if (i != index && vertices[i].sequence > sequence) {
+                    vertices[i] = vertices[i].copy(sequence = vertices[i].sequence + 2)
+                }
+            }
+
+        }
+        _verticeFlow.value = vertices
+
     }
-
-    fun insertVertice(index: Int, location: LatLng) {
-
-        val newList: MutableList<Vertice> = vertices.value.toMutableList()
-        newList.add(index + 1, vertices.value.first().copy(location = location))
-        vertices.value = newList
-    }
-
 }
--- survey/app/src/main/java/com/auterion/tazama/survey/SurveyPolygon.kt
+++ survey/app/src/main/java/com/auterion/tazama/survey/SurveyPolygon.kt
@@ -1,47 +1,99 @@
 package com.auterion.tazama.survey
 
+import android.graphics.PointF
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.core.graphics.minus
+import androidx.core.graphics.plus
 import com.mapbox.mapboxsdk.geometry.LatLng
 import org.maplibre.compose.CircleWithItem
+import org.maplibre.compose.CoordToPixelMaper
 import org.maplibre.compose.MapLibreComposable
+import org.maplibre.compose.PixelToCoordMapper
 import org.maplibre.compose.PolyLine
 import org.maplibre.compose.Polygon
-import org.maplibre.compose.VerticeInsertCalculator
+
+enum class VerticeRole {
+    DRAGGER, INSERTER
+}
 
 data class Vertice(
     val location: LatLng,
     val radius: Float,
     val draggable: Boolean = false,
-    val color: String = "Black"
+    val color: String = "Black",
+    val role: VerticeRole = VerticeRole.DRAGGER,
+    val sequence: Int,
 )
+
+
+@Composable
+fun InsertPointsCalculator(coords: List<LatLng>, onChange: (List<LatLng>) -> Unit) {
+    val pixelCoordinates = remember {
+        mutableStateOf(mutableListOf(PointF()))
+    }
+    CoordToPixelMaper(
+        coordinates = coords.toMutableList(),
+        onChange = { pixelCoordinates.value = it.toMutableList() })
+
+
+    val points = mutableListOf<PointF>()
+
+    pixelCoordinates.value.forEachIndexed { index, point ->
+        var tmp = PointF()
+        if (index < pixelCoordinates.value.size - 1) {
+            tmp = (pixelCoordinates.value[index + 1] - point)
+        } else {
+            // last item
+            tmp = pixelCoordinates.value[0] - point
+        }
+        tmp.x = tmp.x * 0.5f
+        tmp.y = tmp.y * 0.5f
+        points.add(point + tmp)
+    }
+
+    PixelToCoordMapper(points = points, onChange = { onChange(it) })
+
+}
 
 @MapLibreComposable
 @Composable
 fun SurveyPolygon(
     vertices: MutableList<Vertice>,
-    onVerticesChanged: (MutableList<Vertice>) -> Unit,
-    onVerticeAtIndexChanged: (Int, Vertice) -> Unit,
-    onVerticeInsert: (Int, LatLng) -> Unit
+    onVerticesTranslated: (MutableList<LatLng>) -> Unit,
+    onVerticeAtIndexChanged: (Int, LatLng) -> Unit,
 ) {
 
-    val pointsForPolyline: MutableList<LatLng> = vertices.map { it.location }.toMutableList()
+    val pointsForPolyline: MutableList<LatLng> =
+        vertices.sortedBy { it.sequence }.filter { it.role == VerticeRole.DRAGGER }
+            .map { it.location }.toMutableList()
+
     pointsForPolyline.add(vertices.first().location)
 
+    val inserterCoords = remember {
+        mutableStateOf(mutableListOf(LatLng()))
+    }
 
+
+    InsertPointsCalculator(coords = vertices.sortedBy { it.sequence }
+        .filter { it.role == VerticeRole.DRAGGER }
+        .map {
+            it.location
+        }, onChange = { inserterCoords.value = it.toMutableList() }
+    )
 
     Polygon(
         vertices = mutableListOf(
-            vertices.map { it.location }.toMutableList()
+            vertices.sortedBy { it.sequence }.filter { it.role == VerticeRole.DRAGGER }
+                .map { it.location }.toMutableList()
         ),
         fillColor = "Green",
         opacity = 0.3f,
         isDraggable = true,
         onVerticesChanged = {
-            onVerticesChanged(it.first().mapIndexed { index, latLng ->
-                vertices[index].copy(location = latLng)
-            }.toMutableList())
+            onVerticesTranslated(it.first())
+
         })
     PolyLine(
         points = pointsForPolyline,
@@ -51,44 +103,22 @@ fun SurveyPolygon(
 
     vertices.forEachIndexed { index, vertice ->
         CircleWithItem(
-            center = vertice.location,
+            center = when (vertice.role) {
+                VerticeRole.DRAGGER -> vertice.location
+                VerticeRole.INSERTER -> inserterCoords.value[vertice.sequence / 2]
+            },
             radius = vertice.radius,
             isDraggable = vertice.draggable,
-            color = vertice.color,
-            onCenterChanged = { latLng ->
-                onVerticeAtIndexChanged(index, vertice.copy(location = latLng))
-            }
-        )
-    }
-
-    VerticeInserter(vertices = vertices, onVerticeInsert = onVerticeInsert)
-
-}
-
-@Composable
-private fun VerticeInserter(vertices: List<Vertice>, onVerticeInsert: (Int, LatLng) -> Unit) {
-    val insertPoints = remember {
-        mutableStateOf(mutableListOf(LatLng()))
-    }
-    VerticeInsertCalculator(
-        vertices = vertices.map { it.location }.toMutableList(),
-        onPointsChanged = {
-            insertPoints.value = it.toMutableList()
-        })
-
-    insertPoints.value.forEachIndexed { index, latLng ->
-        CircleWithItem(
-            center = latLng,
-            radius = vertices.first().radius,
-            isDraggable = true,
-            color = vertices.first().color,
-            onCenterChanged = {
-                onVerticeInsert(index, it)
+            color = when (vertice.role) {
+                VerticeRole.DRAGGER -> "Gray"
+                VerticeRole.INSERTER -> "Red"
             },
-            imageId = R.drawable.plus,
-            itemSize = 0.5f,
-
-            text = null
+            onCenterChanged = { latLng ->
+                onVerticeAtIndexChanged(index, latLng)
+            },
+            text = vertice.sequence.toString(),
+            itemSize = 12.0f
         )
+
     }
 }
